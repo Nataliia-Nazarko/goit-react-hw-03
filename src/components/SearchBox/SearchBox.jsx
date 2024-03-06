efcc18ac84b9d213b1e317e2e8b78b045cce6a17
Nataliia Nazarko <90257127+Nataliia-Nazarko@users.noreply.github.com>
2024-03-06 20:34:17 +0100

    searchBox return

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,12 @@
+import css from "./SearchBox.module.css";
+
 const SearchBox = () => {
-  return <div></div>;
+  return (
+    <label className={css.searchBox}>
+      <span>Find contacts by name</span>
+      <input type="text" className={css.searchInput} />
+    </label>
+  );
 };
 
 export default SearchBox;
